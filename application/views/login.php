67348df6f8c9c708655f57d8316d42def1a709ef
Chamith Jayaweera <chamithlkml@gmail.com>
2013-01-09 16:17:44 +0530

    codeigniter initial commit

---
+++ application/views/login.php
@@ -0,0 +1,18 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h2>Login Here</h2>
+        <?php
+        // put your code here
+        ?>
+        User name<input type="text" name="username"/>
+    </body>
+</html>
